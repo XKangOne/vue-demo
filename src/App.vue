11f1d6dc89998dffd18b161ae83014cdb217fae7
杨康 <1910297447@qq.com>
2023-10-27 10:35:12 +0800

    修改vuedemo

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,4 @@
 <script setup>
-import { RouterLink, RouterView } from 'vue-router'
-import HelloWorld from './components/HelloWorld.vue'
 </script>
 
 <template>
@@ -8,11 +6,10 @@ import HelloWorld from './components/HelloWorld.vue'
     <img alt="Vue logo" class="logo" src="@/assets/logo.svg" width="125" height="125" />
 
     <div class="wrapper">
-      <HelloWorld msg="你好,欢迎来到vue-demo。。。。。" />
+      <p>欢迎来到vue-demo。。。。。</p>
     </div>
   </header>
 
-  <RouterView />
 </template>
 
 <style scoped>
